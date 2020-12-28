fb97c0cb44a1dab96f0e76e47cb8d0df7cf77925
Josh <anabajoshchi@gmail.com@>
2020-12-28 23:33:46 +0100

    added some styling for form-validation

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  
+   function validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  
+  function validate(){
+         const error = $('#error')
+         const email = $("#email").val();
+         const err =  document.getElementById('error-svg');
+      if(document.emailForm.email.value == ''){
+           error.text('field cannot be empty!');
+           document.emailForm.email.focus();
+           return false;
+       }
+      
+      else  if (!validateEmail(email)){
+         error.css('color', 'red').text(email + " is not a valid email address ");
+         document.emailForm.email.focus();
+         
+        
+             err.style.display = 'block';
+          
+          return false;
+       }
+      else {
+         err.style.display = 'none';
+         error.css('color', 'green').text(email + " is valid ");
+          setTimeout(()=>{
+            alert(`${email} has been added,check your email for more information`)
+          },2000)
+          return true;
+        }
   }
 
-function validate(){
-
-    const email = $("#email").val();
-     if(document.emailForm.email.value == ''){
-         alert('Please provide your email address!');
-         document.emailForm.email.focus();
-         return false;
-     }
-    
-     else  if (!validateEmail(email)){
-        alert(email + " is not valid :(");
-        document.emailForm.email.focus();
-        return false;
-     }
-     else {
-        alert(email + " is valid :)");
-        return true;
-      }
-}
-
-$("#submit-btn").on("click", validate);
-
-
-// window.addEventListener("resize", ()=>{
-//     var image1 = document.getElementById("img").src ="./images/hero-desktop.jpg";
-//     var image2 = document.getElementById("img").src ="./images/hero-mobile.jpg";
-//     var x = window.matchMedia("(max-width: 600px)")
-//     if(x.matches){
-//          image1;
-//     }
-//     else {
-//         image2
-//     };
-// });
-
-
+  $("#submit-btn").on("click", validate);
+  
+document.addEventListener('submit' ,(ev)=>{
+   ev.preventDefault();
+})
+   
